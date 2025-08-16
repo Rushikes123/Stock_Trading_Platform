ee29ffcd2394a08f852cf9b9e1dc12d77d1c8c46
Rushikes123 <153313509+Rushikes123@users.noreply.github.com>
2025-08-16 17:00:27 +0530

    Add files via upload

---
+++ Frontend/src/landing_page/support/SupportPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from './Hero';
+import CreateTicket from './CreateTicket';
+
+
+
+function Support() {
+    return (
+      <>
+      
+       <Hero/>
+      <CreateTicket/>
+      
+       </>
+      );
+}
+
+export default Support;
